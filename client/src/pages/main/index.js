dd8dfed0a0733f53fda7291807d7234a80b773b2
‘Alex’ <oleksii.bashinskii@mirs.com>
2024-06-21 14:08:28 +0300

    sort rozborka

--- client/src/pages/main/index.js
+++ client/src/pages/main/index.js
@@ -1,4 +1,3 @@
-
 // import { CarouselApp } from '../../library'
 import { Carousel } from 'antd';
 
@@ -9,7 +8,19 @@ import img3 from '../../assets/images/sl3.png'
 import { ReactComponent as Point } from '../../assets/icons/point-on-map-svgrepo-com.svg'
 import { ReactComponent as Delivery } from '../../assets/icons/delivery-svgrepo-com.svg'
 
+import mainImg1 from './imgs/1.jpg'
+import mainImg2 from './imgs/2.jpg'
+import mainImg3 from './imgs/3.jpg'
+import mainImg4 from './imgs/4.jpg'
+import mainImg5 from './imgs/5.jpg'
+import mainImg6 from './imgs/6.jpg'
+import mainImg7 from './imgs/7.jpg'
+import mainImg8 from './imgs/8.jpg'
+import mainImg9 from './imgs/9.jpg'
+import mainImg10 from './imgs/10.jpg'
+
 let sliderImages = [img1, img2, img3]
+let mainImages = [mainImg1, mainImg2, mainImg3, mainImg5, mainImg6, mainImg7, mainImg8, mainImg9]
 
 const contentStyle = {
     margin: 0,
@@ -24,11 +35,15 @@ export default function Main(params) {
         console.log(currentSlide);
     };
 
+    const scrollToWorkshop = () => {
+        document.getElementById("workshop-section").scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
         <div>
             <div className="top-section">
                 <div className="top-left-s-left">
-                    <div className="top-left-s-title" >
+                    <div className="top-left-s-title">
                         THE BIKER
                     </div>
                     <div className="top-left-s-subtitle">
@@ -43,9 +58,11 @@ export default function Main(params) {
                             dotsClass="dots-style"
                         >
                             {sliderImages.map((image, i) => {
-                                return (<div key={i} className="main-slider-item">
-                                    <img src={image} alt="" />
-                                </div>)
+                                return (
+                                    <div key={i} className="main-slider-item">
+                                        <img src={image} alt="" />
+                                    </div>
+                                );
                             })}
                         </Carousel>
                     </div>
@@ -58,7 +75,7 @@ export default function Main(params) {
                         На сьогоднішній день ми пропонуємо своїм клієнтам великий асортимент запчастин до популярних <b>японських і європейських</b> мотоциклів
                     </div>
                     <div className="top-right-s-right-text">
-                        <Delivery />  Термін доставки товару, який є в наявності до 2 робочих днів
+                        <Delivery /> Термін доставки товару, який є в наявності до 2 робочих днів
                     </div>
                     <div className="top-right-s-right-text">
                         <Point /> Київ, Софіївська Борщагівка, вул. Соборна 11
@@ -66,10 +83,15 @@ export default function Main(params) {
                 </div>
             </div>
             <div className="bottom-section">
-                <button className="bottom-buttons">виготовлення захисту</button>
-                <button className="bottom-buttons">фарбування та обклеювання мотоциклів</button>
-                <button className="bottom-buttons">ремонт мотоциклів</button>
+                <button className="bottom-buttons" onClick={scrollToWorkshop}>Наша майстерня</button>
+            </div>
+            <div id="workshop-section" className="workshop-section">
+                <div className="workshop-images">
+                    {mainImages.map((image, index) => (
+                        <img key={index} src={image} alt={`mainImg${index + 1}`} className="workshop-image" />
+                    ))}
+                </div>
             </div>
         </div>
-    )
+    );
 }
